6bb4a87fa64e1e0691b594d0e3cd28e56cc784a2
To Merchant <tomerchant@protonmail.com>
2021-05-01 10:07:17 +1000

    First commit

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <img
+                src="http://www.pngall.com/wp-content/uploads/7/Doge-Head-PNG.png"
+            />
+            <p>Random Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
